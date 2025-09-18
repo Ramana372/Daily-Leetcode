b3f686fa154259167ec817d9bb5589e853111edb
Venkata Ramana <118529718+Ramana372@users.noreply.github.com>
2025-09-18 23:07:22 +0530

    Time: 206 ms (55.09%), Space: 41.5 MB (17.9%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        backtrack(board);
+    }
+
+    private boolean backtrack(char[][] board) {
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, r, c, num)) {
+                            board[r][c] = num;
+                            if (backtrack(board)) {
+                                return true;
+                            }
+                            board[r][c] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) return false;
+        }
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) return false;
+        }
+        int startRow = 3 * (row / 3);
+        int startCol = 3 * (col / 3);
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num) return false;
+            }
+        }
+        return true;
+    }
+}
