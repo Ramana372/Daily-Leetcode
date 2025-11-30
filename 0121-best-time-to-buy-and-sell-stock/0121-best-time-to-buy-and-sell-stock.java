aed5a4727de22693956475ba58b0b9f794a60218
Venkata Ramana Sadhanala <118529718+Ramana372@users.noreply.github.com>
2025-11-30 12:15:49 +0530

    Time: 1 ms (99.88%), Space: 94.4 MB (19.25%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,12 +1,25 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        // int m=Integer.MAX_VALUE;
+        // int mp=0;
+        // for(int p:prices){
+        //     if(p<m){
+        //         m=p;
+        //     }
+        //     else if(p-m>=mp){
+        //         mp=p-m;
+        //     }
+        // }
+        // return mp;
+
+
         int m=Integer.MAX_VALUE;
         int mp=0;
         for(int p:prices){
             if(p<m){
                 m=p;
             }
-            else if(p-m>=mp){
+            else if(p-m>mp){
                 mp=p-m;
             }
         }
