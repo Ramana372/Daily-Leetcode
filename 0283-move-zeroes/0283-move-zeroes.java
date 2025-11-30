a63b97fef7fe16067eb3b12273d39d8f6b41131f
Venkata Ramana Sadhanala <118529718+Ramana372@users.noreply.github.com>
2025-11-30 12:12:39 +0530

    Time: 2 ms (84.19%), Space: 47.9 MB (5.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int p=0;
+        // int p=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]!=0){
+        //         int t=nums[p];
+        //         nums[p]=nums[i];
+        //         nums[i]=t;
+        //         p++;
+        //     }
+        // }
+
+        int id=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                int t=nums[p];
-                nums[p]=nums[i];
-                nums[i]=t;
-                p++;
+                nums[id]=nums[i];
+                id++;
             }
+        }
+        while(id<nums.length){
+            nums[id]=0;
+            id++;
         }
     }
 }
